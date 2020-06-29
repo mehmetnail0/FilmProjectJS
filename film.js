7967ee0c580939e28949f720cd85bb01185897c4
lordtigen <54910442+lordtigen@users.noreply.github.com>
2020-06-29 18:29:24 +0300

    Update film.js

--- film.js
+++ film.js
@@ -1,6 +1,14 @@
-// Film Constructor
-function Film(title, director, url) {
-  this.title = title;
-  this.directory = director;
-  this.url = url;
+// // Film Constructor
+// function Film(title, director, url) {
+//   this.title = title;
+//   this.directory = director;
+//   this.url = url;
+// }
+
+class Film {
+  constructor(title, director, url) {
+    this.title = title;
+    this.director = director;
+    this.url = url;
+  }
 }
