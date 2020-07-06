99a2dc74a7a3db8b66393602a8a85b77bb79ce2a
lordtigen <54910442+lordtigen@users.noreply.github.com>
2020-07-06 16:34:27 +0300

    Update project.js

--- project.js
+++ project.js
@@ -1,18 +1,23 @@
 const form = document.getElementById("film-form");
-const titleElement = document.querySelector("#title");
-const directorElement = document.querySelector("#director");
-const urlElement = document.querySelector("#url");
-const cardBody = document.querySelector(".card-body").nextElementSibling;
+const titleElement = document.getElementById("title");
+const directorElement = document.getElementById("director");
+const urlElement = document.getElementById("url");
+const cardBody = document.querySelectorAll(".card-body")[1];
 const clear = document.querySelector("#clear-films");
 
-// Tum Eventleri Yukleme
+// UI objesini baslatma
+const ui = new UI();
+
+// Storage Objesini Baslatma
+const storage = new Storage();
+
 eventListeners();
 
 function eventListeners() {
   form.addEventListener("submit", addFilm);
   document.addEventListener("DOMContentLoaded", function () {
-    let films = Storage.getFilmsFromStorage();
-    UI.loadAllFilms(films);
+    let films = storage.getFilmsFromStorage();
+    ui.loadAllFilms(films);
   });
   cardBody.addEventListener("click", deleteFilm);
   clear.addEventListener("click", clearAllFilms);
@@ -20,35 +25,33 @@ function eventListeners() {
 
 function addFilm(e) {
   const title = titleElement.value;
-  const directory = directorElement.value;
+  const director = directorElement.value;
   const url = urlElement.value;
 
-  if (title === "" || directory === "" || url === "") {
+  if (title === "" || director === "" || url === "") {
     // Hata
-    UI.displayMessages("Tum Alanlari Doldurunuz....", "danger");
+    ui.showAlert("warning", "Lutfen TUM bosluklari doldurunuz");
   } else {
-    // Yeni Film
-    const newFilm = new Film(title, directory, url);
-    UI.addFilmToUI(newFilm);
-    Storage.addFilmToStorage(newFilm);
-    UI.displayMessages("Filminiz Basariyla Eklendi", "success");
+    const newFilm = new Film(title, director, url);
+    ui.addFilmToUI(newFilm);
+    storage.addFilmToStorage(newFilm);
+    ui.showAlert("success", "Film Basariyla Eklendi...");
   }
-  UI.clearInputs(titleElement, urlElement, directorElement);
+  ui.clearInputs(titleElement, directorElement, urlElement);
   e.preventDefault();
 }
 
 function deleteFilm(e) {
   if (e.target.id === "delete-film") {
-    UI.deleteFilmFromUI(e.target);
-    Storage.deleteFilmFromStorage(
+    ui.deleteFilmFromUI(e.target);
+    storage.deleteFilmFromStorage(
       e.target.parentElement.previousElementSibling.previousElementSibling
         .textContent
     );
-    UI.displayMessages("Silme islemi basarili....", "success");
   }
 }
 
 function clearAllFilms() {
-  if (confirm("Emin Misiniz ?")) UI.clearAllFilmsFromUI();
-  Storage.clearAllFilmsFromStorage();
+  ui.clearAllFilmsFromUI();
+  storage.clearAllFilmsFromStorage();
 }
