9d580ab76d64fbb18aa283c1620a2485dbe6f480
lordtigen <54910442+lordtigen@users.noreply.github.com>
2020-07-06 16:33:38 +0300

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,58 +1,46 @@
 class UI {
-  static addFilmToUI(newFilm) {
+  constructor() {}
+  addFilmToUI = (newFilm) => {
     const filmList = document.getElementById("films");
+    filmList.innerHTML += `<tr>
+   <td><img src="${newFilm.url}" class="img-fluid img-thumbnail"></td>
+   <td>${newFilm.title}</td>
+   <td>${newFilm.director}</td>
+   <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
+   </tr>`;
+  };
 
-    filmList.innerHTML += `
-              <tr>
-              <td><img src="${newFilm.url}" class="img-fluid img-thumbnail"></td>
-              <td>${newFilm.title}</td>
-              <td>${newFilm.directory}</td>
-              <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
-            </tr>
-          `;
-  }
-
-  static clearInputs(element1, element2, element3) {
+  clearInputs = (element1, element2, element3) => {
     element1.value = "";
     element2.value = "";
     element3.value = "";
-  }
-  static displayMessages(message, type) {
+  };
+  showAlert = (type, message) => {
     const cardBody = document.querySelector(".card-body");
-    // Alert divini olusturma
-    const div = document.createElement("div");
-    div.className = `alert alert-${type}`;
-    div.textContent = message;
-    cardBody.appendChild(div);
-    setTimeout(function () {
-      div.remove();
-    }, 500);
-  }
-
-  static loadAllFilms(films) {
+    const alert = document.createElement("div");
+    alert.className = `alert alert-${type}`;
+    alert.textContent = message;
+    cardBody.appendChild(alert);
+    setTimeout(() => {
+      cardBody.removeChild(alert);
+    }, 700);
+  };
+  loadAllFilms = (films) => {
     const filmList = document.getElementById("films");
-
-    films.forEach(function (film) {
-      filmList.innerHTML += `
-            <tr>
-              <td><img src="${film.url}" class="img-fluid img-thumbnail"></td>
-              <td>${film.title}</td>
-              <td>${film.directory}</td>
-              <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
-            </tr>`;
+    films.forEach((film) => {
+      filmList.innerHTML += `<tr>
+        <td><img src="${film.url}" class="img-fluid img-thumbnail"></td>
+        <td>${film.title}</td>
+        <td>${film.director}</td>
+        <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
+        </tr>`;
     });
-  }
-
-  static deleteFilmFromUI(element) {
+  };
+  deleteFilmFromUI = (element) => {
     element.parentElement.parentElement.remove();
-  }
-
-  static clearAllFilmsFromUI() {
+  };
+  clearAllFilmsFromUI = () => {
     const filmList = document.getElementById("films");
-    //   filmList.innerHTML = "";
-    while (filmList.firstElementChild !== null) {
-      // Child Oldugu surece
-      filmList.firstElementChild.remove();
-    }
-  }
+    filmList.remove();
+  };
 }
