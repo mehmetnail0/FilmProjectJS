84e6b40d10a3d38e5a58b43e3714135b2e9bba16
lordtigen <54910442+lordtigen@users.noreply.github.com>
2020-07-06 16:33:25 +0300

    Update film.js

--- film.js
+++ film.js
@@ -1,10 +1,3 @@
-// // Film Constructor
-// function Film(title, director, url) {
-//   this.title = title;
-//   this.directory = director;
-//   this.url = url;
-// }
-
 class Film {
   constructor(title, director, url) {
     this.title = title;
